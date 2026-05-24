12273855d51c236bec8c676175349d4f8bfd17bf
ANONYMOUS--SIDD <siddhanttimalsina10@gmai.com>
2026-05-24 12:35:34 +0545

    Modify test Folder

--- flutter_app/test/widget_test.dart
+++ flutter_app/test/widget_test.dart
@@ -8,12 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:innovative_helper/main.dart';
+
+import 'package:tactical_disaster_simulation/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( TacticalDisasterApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
