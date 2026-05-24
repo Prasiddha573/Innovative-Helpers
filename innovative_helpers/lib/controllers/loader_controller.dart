5f9971d7b431a1dade9ade1089f81b2df91b074f
Subash Dhungana <subashd2603@gmail.com>
2026-05-24 13:02:18 +0545

    Delete innovative_helpers directory

--- innovative_helpers/lib/controllers/loader_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-class LoaderController extends GetxController {
-  final isLoading = false.obs;
-  void startLoading() => isLoading.value = true;
-  void stopLoading() => isLoading.value = false;
-}
